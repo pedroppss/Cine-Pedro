05bfc598cbfbc53441c9ab355b01670dcfce79c9
pedropps <realmadrid1999111@gmail.com>
2023-12-21 10:24:53 +0100

    Cine

---
+++ app/views/header.php
@@ -0,0 +1,16 @@
+<header>
+    <img src="app/images/headers/logo.png" alt="logo">
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Movies</li>
+            <li>Series</li>
+            <li>Top</li>
+        </ul>
+    </nav>
+    <div>
+        <img src="app/images/headers/search.png" alt="search">
+        <img src="app/images/headers/notificaciones.png" alt="notificaciones">
+        <img src="app/images/headers/usuario.png" alt="usuario">
+    </div>
+</header>
