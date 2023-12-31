1d1fec9c5aaf7ab3312b8e9316c3d53580f95b7e
pedropps <realmadrid1999111@gmail.com>
2023-12-31 19:50:59 +0100

    informacionPelicula(vista)

---
+++ app/views/informacionPelicula.php
@@ -0,0 +1,37 @@
+<section>
+    <div class="grid grid-cols-2">
+        <div>
+            <a href="index.php?ctl=default"><img src="app/images/informacionPeliculas/vector.png" class="w-6 h-7"></a>
+            <h1 class="p-8 text-white  text-7xl font-poppins font-normal">Titulo</h1>
+            <ul class="flex gap-7 mt-6 ml-8 text-color_gray">
+                <li>8.5</li>
+                <li>2023</li>
+                <li>Action movie</li>
+                <li>2.30</li>
+                <li>16+</li>
+            </ul>
+            <p class="text-white font-normal font-poppins text-sm ml-8 mt-7">sdsfddfrgrthtyjht</p> <!--Argumento-->
+            <div class="flex  gap-2 ml-8 mt-4">
+                <p class="text-color_gray font-normal font-poppins text-sm">Director:</p>
+                <p class="text-white font-normal font-poppins text-sm">James Gunn</p> <!--Nombre_de_Director-->
+            </div>
+            <div class="flex gap-2 ml-8 mt-4">
+                <p class="text-color_gray font-normal font-poppins text-sm">Actors:</p>
+                <p class="text-white font-normal font-poppins text-sm">efewgfregte,sfdsdfdfd,dsgfdfgdgfred,fewdfewferd,</p> <!--Actores-->
+            </div>
+            <div class="grid grid-cols-2 justify-between mt-32 ml-8">
+                    <div class="flex gap-4">
+                        <button class="bg-button_gray w-28 h-11 rounded-md text-white font-medium font-poppins text-base">Trailer</button>
+                        <button class="bg-rose-600 w-28 h-11 rounded-md text-white font-medium font-poppins text-base">Comprar</button>
+                    </div>
+                    <div class="flex gap-4">
+                        <img class="w-8 h-8" src="app/images/informacionPeliculas/gusta.png">
+                        <img class="w-8 h-8" src="app/images/informacionPeliculas/marcador.png">
+                    </div>
+            </div>
+        </div>
+        <div>
+            <img src="app/images/carteles/" class="w-1000 h-620 -ml-193 rounded-md" alt="">
+        </div>
+    </div>
+</section>
