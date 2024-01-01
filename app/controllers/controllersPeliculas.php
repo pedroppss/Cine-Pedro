e723c5a190befc3cc142b3f61103104d0909b674
pedropps <realmadrid1999111@gmail.com>
2024-01-01 22:44:42 +0100

    controllersPeliculas.php

--- app/controllers/controllersPeliculas.php
+++ app/controllers/controllersPeliculas.php
@@ -1,9 +1,24 @@
 <?php
 include "app/models/peliculas.php";
 class controllersPeliculas{
-    public function mostrar(){
-        $idFoto=(isset($_GET["id"]) ? intval($_GET["id"]):0);
-        $peli=new peliculas();
-        $peli->mostrarImagenes($idFoto);
+    public function mostrar_fast_furious(){
+        //$id=(isset($_GET["id"]) ? intval($_GET["id"]):0);
+        //if($id==2){
+            //isset($_SESSION)?:session_start();
+            $peli=new peliculas();
+            $_SESSION['peliculas']=$peli->mostrarImagenes(2);
+            //unset($_SESSION['peliculas']);
+            //var_dump($_SESSION['peliculas']);
+            //unset($_SESSION['peliculas']);
+            //header("");
+            //exit();      
+       
     }
+    public function mostrar_air()
+    {
+            $peli=new peliculas();
+            $_SESSION['peliculas']=$peli->mostrarImagenes(3);
+       
+    }
+    
 }
