5e1d0f142bea88063a4bcd1aaf215f05d4a63f60
pedropps <realmadrid1999111@gmail.com>
2024-01-01 22:44:57 +0100

    peliculas.php

--- app/models/peliculas.php
+++ app/models/peliculas.php
@@ -11,23 +11,39 @@ class peliculas extends conexion_2{
         {
             $instancia=new peliculas();
             $conexion=$instancia->conexion;
-            $consultasql="select peliculasc.id,peliculasc.nombre,peliculasc.cartel,peliculasc.clasificacion_edad, generoc.nombre from peliculasc LEFT join generoc on peliculasc.genero_id=generoc.id WHERE peliculasc.id=:id;";
+            $consultasql="select peliculasc.id,peliculasc.clasificacion as clasif,peliculasc.año as año, peliculasc.duracion as duracion,peliculasc.argumento as argumento,peliculasc.nombre as nombre ,peliculasc.cartel as cartel ,peliculasc.clasificacion_edad as edad, generoc.nombre as genero from peliculasc LEFT join generoc on peliculasc.genero_id=generoc.id WHERE peliculasc.id=:id;";
             $enlaceDatos=$conexion->prepare($consultasql);
             $enlaceDatos->bindParam(":id",$id,PDO::PARAM_INT);
             $enlaceDatos->execute();
+
+            unset($_SESSION['peliculas']);
+
             while($row = $enlaceDatos->fetch(PDO::FETCH_ASSOC)){
-                $genero=$row["generoc.nombre"];
-                $imagen=$row["peliculasc.cartel"];
-                $edad=$row['peliculasc.clasificacion_edad'];
+                $titulo=$row["nombre"];
+                $genero=$row["genero"];
+                $imagen=$row["cartel"];
+                $edad=$row['edad'];
+                $argumento=$row["argumento"];
+                $clasif=$row["clasif"];
+                $año=$row["año"];
+                $duracion=$row["duracion"];
+                //Agregar informacion a las variables de sesión.
                 $_SESSION['peliculas']['genero'][]=$genero;
                 $_SESSION['peliculas']['imagen'][]=$imagen;
                 $_SESSION['peliculas']['edad'][]=$edad;
+                $_SESSION['peliculas']['nombre'][]=$titulo;
+                $_SESSION['peliculas']['argumento'][]=$argumento;
+                $_SESSION['peliculas']['clasificacion'][]=$clasif;
+                $_SESSION['peliculas']['año'][]=$año;
+                $_SESSION['peliculas']['duracion'][]=$duracion;
+
             }
             $conexion=null;
+            return $_SESSION['peliculas'];
             
 
         }catch(PDOException $e){
-            $e->getMessage();
+            echo "Error:" .$e->getMessage();
         }
     }
 }
