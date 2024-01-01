9e19cd3bc8512fc4ce409e8d4ae77aefbe5fe333
pedropps <realmadrid1999111@gmail.com>
2024-01-01 22:45:08 +0100

    informacionPelicula.php

--- app/views/informacionPelicula.php
+++ app/views/informacionPelicula.php
@@ -1,16 +1,19 @@
+<?php
+session_start();
+?>
 <section>
     <div class="grid grid-cols-2">
         <div>
             <a href="index.php?ctl=default"><img src="app/images/informacionPeliculas/vector.png" class="w-6 h-7"></a>
-            <h1 class="p-8 text-white  text-7xl font-poppins font-normal">Titulo</h1>
+            <h1 class="p-8 text-white  text-7xl font-poppins font-normal"><?=$_SESSION['peliculas']['nombre'][0]?></h1>
             <ul class="flex gap-7 mt-6 ml-8 text-color_gray">
-                <li>8.5</li>
-                <li>2023</li>
-                <li>Action movie</li>
-                <li>2.30</li>
-                <li>16+</li>
+                <li><?=$_SESSION['peliculas']['clasificacion'][0]?></li>
+                <li><?=$_SESSION['peliculas']['año'][0]?></li>
+                <li><?=$_SESSION['peliculas']['genero'][0]?></li>
+                <li><?=$_SESSION['peliculas']['duracion'][0]?></li>
+                <li><?=$_SESSION['peliculas']['edad'][0]?></li>
             </ul>
-            <p class="text-white font-normal font-poppins text-sm ml-8 mt-7">sdsfddfrgrthtyjht</p> <!--Argumento-->
+            <p class="text-white font-normal font-poppins text-sm ml-8 mt-7"><?=$_SESSION['peliculas']['argumento'][0]?></p> <!--Argumento-->
             <div class="flex  gap-2 ml-8 mt-4">
                 <p class="text-color_gray font-normal font-poppins text-sm">Director:</p>
                 <p class="text-white font-normal font-poppins text-sm">James Gunn</p> <!--Nombre_de_Director-->
@@ -31,7 +34,7 @@
             </div>
         </div>
         <div>
-            <img src="app/images/carteles/" class="w-1000 h-620 -ml-193 rounded-md" alt="">
+            <img src="app/images/carteles/<?=$_SESSION['peliculas']['imagen'][0]?>" class="w-1000 h-620 -ml-151 rounded-md" alt="">
         </div>
     </div>
 </section>
