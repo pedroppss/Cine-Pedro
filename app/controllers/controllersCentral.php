1bf8e6edaebb7990e39a306eeb645ce64323474d
pedropps <realmadrid1999111@gmail.com>
2024-01-01 22:44:29 +0100

    controllersCentral.php

--- app/controllers/controllersCentral.php
+++ app/controllers/controllersCentral.php
@@ -2,9 +2,13 @@
     include "controllersPeliculas.php";
     $accion=$_REQUEST['ctl'] ?? 'default';
     switch ($accion) {
-        case 'id':
+        case 'id=2':
             include "app/views/informacionPelicula.php";
-           //(new controllersPeliculas())->mostrar();
+           (new controllersPeliculas())->mostrar_fast_furious();
+            break;
+        case 'id=3':
+            include "app/views/informacionPelicula.php";
+           (new controllersPeliculas())->mostrar_air();
             break;
         case 'register':
 
@@ -14,6 +18,7 @@
             break;
         default:
             include "app/views/inicio.php";
+            //unset($_SESSION['peliculas']);
             break;
     }
 ?>
