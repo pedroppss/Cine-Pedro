812c91089885913526b344c15dc3c0adf4d3d601
pedropps <realmadrid1999111@gmail.com>
2024-01-04 22:17:24 +0100

    salaButacas.php

---
+++ app/views/salaButacas.php
@@ -0,0 +1,18 @@
+<a href="index.php?ctl=default"><img src="app/images/salaButacas/vector.png" class="w-6 h-7 ml-8"></a>
+<section class="bg-fond_inicio rounded-2xl mt-12 mb-16 h-72 ml-8">
+    <div class="grid grid-cols-2 p-8">
+        <div class="">
+            <button>Sala 1</button>
+            <button>Sala 2</button>
+        </div>
+        <div>
+            <p>Sala 1 - 16:30h</p>
+            <p>Elige tus entradas</p>
+        </div>
+    </div>
+    <div>
+        <p>16:30</p>
+        <p>18:30</p>
+        <p>21:15</p>
+    </div>
+</section>
