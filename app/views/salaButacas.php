35b64b9482f7cd989f17ca94b06d92dd0c1920f3
pedropps <realmadrid1999111@gmail.com>
2024-01-08 13:12:55 +0100

    sala_Butacas.php

--- app/views/salaButacas.php
+++ app/views/salaButacas.php
@@ -3,7 +3,7 @@
     <div class="grid grid-cols-2 p-8 text-center">
         <div class="flex gap-12 ml-56">
             <button class="bg-button_gray_3 w-28 h-14 text-white font-medium font-poppins text-lg rounded-md">Sala 1</button>
-            <button class="bg-button_gray_4 w-28 h-14 text-white font-medium font-poppins text-lg rounded-md">Sala 2</button>
+            <a href="index.php?ctl=butacas_2" class="bg-button_gray_4 w-28 h-14 text-white font-medium font-poppins text-lg rounded-md" type="button"><p class="mt-3">Sala 2</p></a>
         </div>
         <div>
             <p class="text-white font-medium text-3xl font-poppins ">Sala 1 - 16:30h</p>
@@ -62,4 +62,154 @@
             <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
         </div>
     </div>
+    <div class="grid grid-cols-2 ml-80 mt-5">
+        <div class="flex gap-2">
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+        </div>
+        <div class="flex gap-2 ml-32">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 ml-80 mt-5">
+        <div class="flex gap-2">
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+        </div>
+        <div class="flex gap-2 ml-32">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 ml-80 mt-5">
+        <div class="flex gap-2">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+        </div>
+        <div class="flex gap-2 ml-32">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 ml-80 mt-5">
+        <div class="flex gap-2">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+        </div>
+        <div class="flex gap-2 ml-32">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+        </div>
+    </div>
+    <div class="grid grid-cols-2 ml-80 mt-5">
+        <div class="flex gap-2">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+        </div>
+        <div class="flex gap-2 ml-32">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+        </div>
+    </div>
+    <div class="flex gap-2 mt-5 ml-40">
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca2.png" class="w-11 h-10"></a>
+            <a href=""><img src="app/images/salaButacas/butaca1.png" class="w-11 h-10"></a>
+    </div>
+</section>
+<section>
+    <div class="flex gap-20  justify-center mt-20 mb-20">
+        <div>
+            <img src="app/images/salaButacas/Unavailable.png" class="w-11 h-10">
+            <p class="text-white font-poppins font-normal text-sm">Unavailable</p>
+        </div>
+        <div>
+            <img src="app/images/salaButacas/Available.png" class="w-11 h-10">
+            <p class="text-white font-poppins font-normal text-sm">Available</p>
+        </div>
+        <div>
+            <img src="app/images/salaButacas/Selected.png" class="w-11 h-10">
+            <p class="text-white font-poppins font-normal text-sm">Selected</p>
+        </div>
+    </div>
+    <a href="index.php?ctl=factura" class="bg-p_rose w-1500 h-87 ml-8 mt-20 mb-16 rounded-md text-white text-3xl" type="button"><p class="text-center mt-5">Comprar</p></a>
 </section>
