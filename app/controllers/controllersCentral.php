a28dc949221280c8c6f0a9f8d07128fa884dcb8a
pedropps <realmadrid1999111@gmail.com>
2024-01-08 13:12:22 +0100

    controllersCentral.php

--- app/controllers/controllersCentral.php
+++ app/controllers/controllersCentral.php
@@ -46,6 +46,12 @@
         case 'butacas':
             include "app/views/salaButacas.php";
             break;
+        case 'butacas_2':
+            include "app/views/salaButacas2.php";
+            break;
+        case 'factura':
+            include "app/views/facturaEntrada.php";
+            break;
         case 'register':
             break;
         case 'inicio_2':
