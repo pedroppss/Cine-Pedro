ad86643677bf6827c3c9515878d4cd202029adad
pedropps <realmadrid1999111@gmail.com>
2024-01-09 13:29:40 +0100

    pagAdmin.php

--- app/views/pagadmin.php
+++ app/views/pagadmin.php
@@ -9,10 +9,12 @@
 <body class="container max-w-screen-2xl mx-auto bg-fond_black">
    <header class="container max-w-screen-2xl p-11 grid grid-cols-2">
         <img src="../images/login_register/logo.png" style="width: 179.23px ; height: 55px;" alt="logo">
-        <div class="flex justify-end text-white font-normal font-poppins gap-6 text-lg">
+        <div class="flex justify-end text-white font-normal font-poppins gap-6 text-sm">
             <img src="../images/avatar/<?php $_SESSION['usuarios']['avatar'][0]?>">
             <a href="login_register.php?ctl=logout">Cerrar Sesion</a>
-            <a href="#">Añadir una nueva pelicula</a>
+            <a href="login_register.php?ctl=añadir">Añadir una nueva pelicula</a>
+            <a href="login_register.php?ctl=eliminar">Eliminar una pelicula</a>
+            <a href="login_register.php?ctl=editar">Editar una pelicula</a>
         </div>
    </header>
    <main class="bg-no-repeat bg-personalized h-personalized grid content-around justify-around" style="background-image: url(../images/login/fondo-imagenes.png);">
