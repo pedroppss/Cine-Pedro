26b3f3d5265d1641a6607855dc6cbd95133a8952
pedropps <realmadrid1999111@gmail.com>
2024-01-11 13:29:56 +0100

    usuario.php

--- app/models/usuario.php
+++ app/models/usuario.php
@@ -115,5 +115,161 @@ class Usuario extends conexion{
             exit("Error:" .$e->getMessage());
         }
    }
+   public function listarPeliculas()
+   {
+       try
+       {
+       $instancia=new Usuario();
+       $conexion=$instancia->conexion;
+       $consultasql="select peliculasc.clasificacion as clasif,peliculasc.año as año, peliculasc.duracion as duracion,
+           peliculasc.argumento as argumento,peliculasc.nombre as nombre ,peliculasc.cartel as cartel ,peliculasc.clasificacion_edad as edad from peliculasc";
+       $enlaceDatos=$conexion->prepare($consultasql);
+       $enlaceDatos->execute();
+       $peliculas = array();
+       while($row = $enlaceDatos->fetch(PDO::FETCH_ASSOC)){
+       $pelicula =array(
+           'titulo'=>$row['nombre'],
+           'imagen' => $row["cartel"],
+           'edad' => $row['edad'],
+           'argumento' => $row["argumento"],
+           'clasif' => $row["clasif"],
+           'año' => $row["año"],               
+           'duracion' => $row["duracion"]
+          );
+       $peliculas[] = $pelicula;
+       }
+       $_SESSION['peliculas']=$peliculas;
+
+       $conexion=null;
+       return $_SESSION['peliculas'];
+   }catch(PDOException $e){
+           echo "Error:" .$e->getMessage();
+       }
+
+   } 
+   public function listarUsuarios()
+   {
+       try
+       {
+       $instancia=new Usuario();
+       $conexion=$instancia->conexion;
+       $consultasql="select usuariosc.nombre as nombre, usuariosc.apellidos as apellidos, usuariosc.avatar as avatar, usuariosc.NIF as nif,
+       usuariosc.hash_pass as contraseña, usuariosc.correo as correo, usuariosc.rol as rol from usuariosc";
+       $enlaceDatos=$conexion->prepare($consultasql);
+       $enlaceDatos->execute();
+       $usuarios = array();
+       while($row = $enlaceDatos->fetch(PDO::FETCH_ASSOC)){
+       $usuario =array(
+           'nombre'=>$row['nombre'],
+           'avatar' => $row["avatar"],
+           'apellidos' => $row['apellidos'],
+           'nif' => $row["nif"],
+           'contraseña' => $row["contraseña"],
+           'rol' => $row["rol"],               
+           'correo' => $row["correo"]
+          );
+       $usuarios[] = $usuario;
+       }
+       $_SESSION['usuarios']=$usuarios;
+
+       $conexion=null;
+       return $_SESSION['usuarios'];
+   }catch(PDOException $e){
+           echo "Error:" .$e->getMessage();
+       }
+
+   } 
+   public function listarActores()
+   {
+       try
+       {
+           $tipo="actor";
+           $instancia=new Usuario();
+           $conexion=$instancia->conexion;
+           $consultasql="select personalc.nombre as nombre, 
+            personalc.imagen as imagen , personalc.tipo as tipo, peliculasc.nombre as titulo from peliculasc INNER JOIN peliculas_personalc on peliculasc.id=peliculas_personalc.pelicula_id 
+                INNER JOIN personalc on personalc.id=peliculas_personalc.personal_id where personalc.tipo='actor'";
+           $enlaceDatos=$conexion->prepare($consultasql);
+           $enlaceDatos->execute();
+    
+            $enlaceDatos->bindParam(":tipo",$tipo,PDO::PARAM_STR);
+           $actores=array();
+           while($row = $enlaceDatos->fetch(PDO::FETCH_ASSOC)){
+            $actor =array(
+                'nombre'=>$row['nombre'],
+                'foto' => $row["imagen"],
+                'tipo' => $row['tipo'],
+                'tituloPelicula' => $row["titulo"],
+        
+            );
+            $actores[]=$actor;
+           }
+           $_SESSION['actores']=$actores;
+           $conexion=null;
+           return $_SESSION['actores'];
+       }catch(PDOException $e){
+           echo "Error:" .$e->getMessage();
+       }
+   }
+   public function listarActrices()
+   {
+       try
+       {
+           
+           $instancia=new Usuario();
+           $conexion=$instancia->conexion;
+           $consultasql="select personalc.nombre as nombre, 
+            personalc.imagen as imagen , personalc.tipo as tipo, peliculasc.nombre as titulo from peliculasc INNER JOIN peliculas_personalc on peliculasc.id=peliculas_personalc.pelicula_id 
+                INNER JOIN personalc on personalc.id=peliculas_personalc.personal_id where personalc.tipo='actriz'";
+           $enlaceDatos=$conexion->prepare($consultasql);
+           $enlaceDatos->execute();
+           $actrices=array();
+           while($row = $enlaceDatos->fetch(PDO::FETCH_ASSOC)){
+            $actriz =array(
+                'nombre'=>$row['nombre'],
+                'foto' => $row["imagen"],
+                'tipo' => $row['tipo'],
+                'tituloPelicula' => $row["titulo"],
+        
+            );
+            $actrices[]=$actriz;
+           }
+           $_SESSION['actrices']=$actrices;
+           $conexion=null;
+           return $_SESSION['actrices'];
+       }catch(PDOException $e){
+           echo "Error:" .$e->getMessage();
+       }
+   }
+   public function listarDirector()
+   {
+       try
+       {
+           
+           $instancia=new Usuario();
+           $conexion=$instancia->conexion;
+           $consultasql="select personalc.nombre as nombre, 
+            personalc.imagen as imagen , personalc.tipo as tipo, peliculasc.nombre as titulo from peliculasc INNER JOIN peliculas_personalc on peliculasc.id=peliculas_personalc.pelicula_id 
+                INNER JOIN personalc on personalc.id=peliculas_personalc.personal_id where personalc.tipo='director'";
+           $enlaceDatos=$conexion->prepare($consultasql);
+           $enlaceDatos->execute();
+           $directores=array();
+           while($row = $enlaceDatos->fetch(PDO::FETCH_ASSOC)){
+            $director =array(
+                'nombre'=>$row['nombre'],
+                'foto' => $row["imagen"],
+                'tipo' => $row['tipo'],
+                'tituloPelicula' => $row["titulo"],
+        
+            );
+            $directores[]=$director;
+           }
+           $_SESSION['directores']=$directores;
+           $conexion=null;
+           return $_SESSION['directores'];
+       }catch(PDOException $e){
+           echo "Error:" .$e->getMessage();
+       }
+   }
 }
 ?>
