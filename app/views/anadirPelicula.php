8d6ad300b5429b82dd9f31dbe68c7c25c4988bab
pedropps <realmadrid1999111@gmail.com>
2024-01-11 13:30:05 +0100

    añadirPelicula.php

--- app/views/anadirPelicula.php
+++ app/views/anadirPelicula.php
@@ -1,3 +1,4 @@
+<body class="container max-w-screen-2xl mx-auto bg-fond_black">
 <main class="bg-no-repeat bg-personalized h-personalized grid content-around justify-around mt-[103px]" style="background-image: url(../images/login/fondo-imagenes.png);">
                
                 <div class="p-24 w-500 h-[571px] rounded-2xl bg-fond_transp"> 
@@ -22,3 +23,4 @@
                     </form>
                 </div>      
     </main>
+</body>
