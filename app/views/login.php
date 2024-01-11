69213f4b2ecfd393abbadcd2226e5f11c40297df
pedropps <realmadrid1999111@gmail.com>
2024-01-11 13:30:34 +0100

    login

--- app/views/login.php
+++ app/views/login.php
@@ -12,6 +12,7 @@
         <img src="../images/login/logo.png" style="width: 179.23px ; height: 55px;" alt="logo">
    </header> 
 -->
+<body class="container max-w-screen-2xl mx-auto bg-fond_black">
    <main class="bg-no-repeat bg-personalized h-personalized grid content-around justify-around" style="background-image: url(../images/login/fondo-imagenes.png);">
             <div class="p-24 w-500 h-516 rounded-2xl bg-fond_transp">
                     <form action="login_register.php?ctl=login" method="POST">
@@ -27,6 +28,7 @@
                     </form>
             </div>
     </main>
+</body>
 <!--
 </body>
 </html>
